2762b66774990d1c14a71b530b80c4b17752fbc6
LamGia <laml33366@gmail.com>
2024-08-18 23:20:25 +0700

    upload src code

---
+++ main.js
@@ -0,0 +1,40 @@
+const yes = document.getElementById('yes');
+const no = document.getElementById('no');
+let modalContent = document.querySelector(".modal-content");
+let blurBg = document.querySelector(".blur-bg");
+let closeModal = document.querySelector(".close-modal");
+let closePopup = document.querySelector(".close-popup");
+no.addEventListener('mouseover', ()=> {
+    const x = Math.floor(Math.random() * (window.innerHeight-150));
+    const y =  Math.floor(Math.random() * (window.innerHeight-150));
+    no.style.left = `${x}px`;
+    no.style.top = `${y}px`;
+
+});
+no.addEventListener('mouseover', function(){
+    no.style.transform = 'scale(0.5)';
+});
+// no.addEventListener('mouseout', function(){
+//     no.style.transform = 'scale(1)';
+// });
+yes.addEventListener('click',  function(){
+    modalContent.classList.remove("hidden-modal");
+    blurBg.classList.remove("hidden-blur");
+})
+let closeModalFunction = function () {
+    modalContent.classList.add("hidden-modal")
+    blurBg.classList.add("hidden-blur")
+}
+blurBg.addEventListener("click", closeModalFunction);
+closeModal.addEventListener("click", function(){
+    alert('Giờ Em Sẽ Là Của Anh 3==D');
+});
+closeModal.addEventListener("click", closeModalFunction);
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape"
+    && !modalContent.classList.contains("hidden")
+    ) {
+    closeModalFunction();
+    }
+});
